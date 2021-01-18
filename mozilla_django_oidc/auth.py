84182b00e8ae909c9fa38e782659682cd0ca3f04
Roger Bertilsson <rogerbn@axis.com>
2021-01-18 12:24:32 +0100

    Fixed the Basic authorization use case
    which was destroyed in previous commit

--- mozilla_django_oidc/auth.py
+++ mozilla_django_oidc/auth.py
@@ -268,6 +268,7 @@ class OIDCAuthenticationBackend(ModelBackend):
 
         token_payload = {
             'client_id': self.OIDC_RP_CLIENT_ID,
+            'client_secret': self.OIDC_RP_CLIENT_SECRET,
             'code_verifier': code_verifier,
             'grant_type': 'authorization_code',
             'code': code,
